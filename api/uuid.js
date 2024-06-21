6f30bb289cfdb56d920ad2fffee8d2c075af5c6b
memiscavage <153465556+memiscavage@users.noreply.github.com>
2024-06-21 12:20:52 -0500

    Update uuid.js
    added note in code for reference

--- api/uuid.js
+++ api/uuid.js
@@ -1,4 +1,4 @@
-// From Module 11 solved student mini project
+// From Module 11 solved student custom middleware activity
 // Generates random string for the id on new notes
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
