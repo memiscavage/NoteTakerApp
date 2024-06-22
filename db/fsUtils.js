53c53f1d7f0dea49847ca469b93fac7090b5c34e
Mariel Miscavage <memiscavage@gmail.com>
2024-06-22 13:13:50 -0500

    renamed folder and deleted unused files

--- db/fsUtils.js
+++
@@ -1,3 +0,0 @@
-// TODO: Delete this file
-// const fs = require('fs');
-// const util = require('util');
---
+++ helpers/uuid.js
@@ -0,0 +1,8 @@
+// From Module 11 solved student custom middleware activity
+// Generates random string for the id on new notes
+module.exports = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+//rename parent folder to helpers
